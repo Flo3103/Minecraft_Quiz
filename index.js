67dbf7fc700572bcb40083ab63b1d60c7d410c59
Flo3103 <florian.hoeck4@gmail.com>
2022-03-08 20:35:19 +0100

    2.0

--- index.js
+++ index.js
@@ -2,3 +2,17 @@ function StartButton() {
     document.getElementById('StartButtonDiv').style.display = 'none'; 
     document.getElementById('LevelChoiceDiv').style.display = 'block';
 }
+
+
+
+
+function ChoiceRight() {
+    document.getElementById('correctionP').innerHTML = 'Richtig';
+    document.getElementById('ChoiceDiv').style.display = 'none';
+    document.getElementById('correctionDiv').style.display = 'flex';
+    document.getElementById('NächsteFrageDiv').style.display = 'flex';
+}
+
+function ChoiceFals() {
+    document.getElementById('correctionP').innerHTML = 'Falsch';
+}
