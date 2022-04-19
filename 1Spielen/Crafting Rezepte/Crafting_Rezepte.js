ca602c961e21debd9b312725f46183f0563dbe8a
Flo3103 <florian.hoeck4@gmail.com>
2022-04-19 18:13:03 +0200

    6.5.1

--- 1Spielen/Crafting Rezepte/Crafting_Rezepte.js
+++ 1Spielen/Crafting Rezepte/Crafting_Rezepte.js
@@ -1,13 +1,53 @@
+var CraftingChoiceR = 0;
+var CraftingChoiceF = 0;
 
-function CraftingRight() {
-    document.getElementById('Crafting_RightOrFals').style.display = 'block';
-    document.getElementById('Crafting_Correction').style.display = 'block';
+var Rezepte = new Array();
 
-    document.getElementById('NächsteFrageDiv').style.display = 'flex';
+function CraftingWhatsRight() {
+    if(CraftingChoiceR === 1) {
+        document.getElementById('Crafting_Right1').style.display = 'block';
+        document.getElementById('Crafting_Choice1').style.border = '3px solid green';
+        document.getElementById('Crafting_Fals1').style.display = 'none';
+    }
+    if(CraftingChoiceR === 2) {
+        document.getElementById('Crafting_Right2').style.display = 'block';
+        document.getElementById('Crafting_Choice2').style.border = '3px solid green';
+        document.getElementById('Crafting_Fals2').style.display = 'none';
+    }
+    if(CraftingChoiceR === 2) {
+        document.getElementById('Crafting_Right3').style.display = 'block';
+        document.getElementById('Crafting_Choice3').style.border = '3px solid green';
+        document.getElementById('Crafting_Fals3').style.display = 'none';
+    }
 
 }
 
+
+function CraftingRight() {
+    CraftingWhatsRight();
+    //document.getElementById('Crafting_RightOrFals').style.display = 'block';
+    document.getElementById('Crafting_Correction').style.display = 'block';
+    document.getElementById('NächsteFrageDiv').style.display = 'flex';
+    
+}
+
+
 function CraftingFals() {
-    document.getElementById('1').src = 'https://www.hofbraeu-wirtshaus.de/wp-content/uploads/2016/05/button-stammtische.jpg';
+    document.getElementById('Crafting_Correction').style.display = 'block';
+    document.getElementById('Crafting_Fals1').style.display = 'block'
+    document.getElementById('Crafting_Fals2').style.display = 'block'
+    document.getElementById('Crafting_Fals3').style.display = 'block'
+    document.getElementById('Crafting_Choice1').style.border = '3px solid red';
+    document.getElementById('Crafting_Choice2').style.border = '3px solid red';
+    document.getElementById('Crafting_Choice3').style.border = '3px solid red';
+    CraftingWhatsRight();
+}
+
+
+
+
+
+
+function Rezept2() {
     document.getElementById('Crafting').style.display = 'none';
 }
