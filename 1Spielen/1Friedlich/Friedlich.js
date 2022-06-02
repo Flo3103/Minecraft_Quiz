a4db43ac9966ae79ec89013ca6b6e15e64ce23d3
Flo3103 <93220348+Flo3103@users.noreply.github.com>
2022-06-02 10:26:44 +0200

    1.1

--- 1Spielen/1Friedlich/Friedlich.js
+++ 1Spielen/1Friedlich/Friedlich.js
@@ -21,18 +21,26 @@ function returnPunkte() {
 }
 
 function Friedlich_Choice(gewählteAntwort) {
-    
-    document.getElementById('correctionDiv').style.display = 'none';
-    document.getElementById('NächsteFrageDiv').style.display = 'none';
-
     if(Buttongesperrt == true){ButtonClickSound.src = ''; return}
     Buttongesperrt = true;
     
     if(gewählteAntwort.getAttribute('id') == richtigeAntwort){
-     
+        FriedlichWhatsRight();
+    }else{
+        if(richtigeAntwort == 'Überleben_Choice2&3'){
+            document.getElementById('Überleben_Choice2').style.border = '2px solid rgb(13, 241, 13)';
+            document.getElementById('Überleben_Choice3').style.border = '2px solid rgb(13, 241, 13)';
+            document.getElementById('NächsteFrageDiv').style.display = 'flex';
+            return
+        }
+        document.getElementById('Choice1').style.borderColor = 'red;
+        document.getElementById('Choice2').style.borderColor = 'red;
+        document.getElementById('Choice3').style.borderColor = 'red;
+        FriedlichWhatsRight();
+        ButtonClickSound.src = '';
+        DamageSound.play();
         
-        
-    }  
+      }  
     
     
 }
